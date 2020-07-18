3a586dee2a41c116dbc742cb5b9a4892ab6b54e6
martinDM <hello@martindm.uk>
2020-07-18 01:18:51 +0100

    Removing sandbox code

--- src/main-page/app-presentation.js
+++ src/main-page/app-presentation.js
@@ -4,11 +4,6 @@ import SearchResults from '../search-results';
 import HouseDetail from '../house';
 import Header from './header';
 import HouseFilter from './house-filter';
-
-import About from './About';
-import Article from './Article';
-import Shop from './Shop';
-import Nav from './Nav';
 import { BrowserRouter as Router , Link, Switch, Route} from 'react-router-dom'
 
 const AppPresentation = (props) => {
@@ -27,29 +22,14 @@ const AppPresentation = (props) => {
   return (
     <Router>
       <div className="container">
-      <Nav />
       <Header subtitle="Nice moves" />
       <HouseFilter filterHouses={props.filterHouses}
       countries={props.countries} />
       {activeComponent}
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route exact path="/shop" component={Shop} />
-        <Route path="/shop/:name" component={Article} />
-      </Switch>
     </div> 
     </Router>
 
    );
 }
-
-export const Home = () => {
-  return ( 
-    <div>
-      <h1>Home</h1>
-    </div>
-    );
-} 
  
 export default AppPresentation;
